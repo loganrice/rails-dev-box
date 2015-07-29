cd4956edc48be8154a974e43b49a0708495ee965
Bradley Giesbrecht <brad@pixilla.com>
2015-07-29 11:03:18 -0700

    Fix Vagrantfile and provisioning script. Nothing is installed into $HOME any longer.

--- Vagrantfile
+++ Vagrantfile
@@ -1,22 +1,10 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
+Vagrant.configure('2') do |config|
+  config.vm.box      = 'ubuntu/trusty64'
+  config.vm.hostname = 'rails-dev-box'
 
-# All Vagrant configuration is done below. The "2" in Vagrant.configure
-# configures the configuration version (we support older styles for
-# backwards compatibility). Please don't change it unless you know what
-# you're doing.
-Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.network :private_network, ip: "10.9.8.7"
 
-  config.vm.hostname = "rails"
-  config.vm.network :private_network, ip: "192.168.33.8"
-
-  config.vm.provider "virtualbox" do |v|
-    v.customize ["modifyvm", :id, "--memory", "1024", "--cpuexecutioncap", "50"]
-  end
-
-  # Use NFS for shared folders for better performance
-  config.vm.synced_folder '.', '/vagrant', nfs: true
-
-  config.vm.provision :shell, path: 'rails-env-provision.sh', keep_color: true
+  config.vm.provision :shell, path: 'rails-env-provision.sh'
 end
--- rails-env-provision.sh
+++ rails-env-provision.sh
@@ -1,85 +1,54 @@
 # The output of all these installation steps is noisy. With this utility
 # the progress report is nice and concise.
 
+OUT=''
+#OUT='>/dev/null 2>&1'
+
 function install {
-    echo installing $@
-    echo sudo apt-get -y install "$@"
-    sudo apt-get -y install "$@"
+    echo installing $1
+    shift
+    apt-get -y install "$@" ${OUT}
 }
 
+install 'development tools' build-essential
+install 'apt-get transport' apt-transport-https ca-certificates
+
+echo adding key for passenger
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
+
+echo updating package information
+echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main' \
+    > /etc/apt/sources.list.d/passenger.list
+apt-add-repository -y ppa:brightbox/ruby-ng ${OUT}
+add-apt-repository -y ppa:chris-lea/node.js ${OUT}
+add-apt-repository -y ppa:nginx/stable ${OUT}
+apt-get -y update ${OUT}
 
 echo installing core libraries
-install git-core
-install curl
-install zlib1g-dev
-install build-essential
-install libssl-dev
-install libreadline-dev
-install libyaml-dev
-install sqlite3
-install libsqlite3-dev
-install libxml2-dev
-install libxslt1-dev
-install libcurl4-openssl-dev
-install python-software-properties
-install libffi-dev
-install imagemagick
-install libmagickwand-dev
-apt-get update
+install Libs zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev \
+        python-software-properties libffi-dev
 
-echo installing ruby
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+install Ruby ruby2.2 ruby2.2-dev
+update-alternatives --set ruby /usr/bin/ruby2.2 ${OUT}
+update-alternatives --set gem /usr/bin/gem2.2 ${OUT}
 
-# Install ruby 2.1.6 and bundler
-export RBENV_ROOT="${HOME}/.rbenv"
-export PATH="${RBENV_ROOT}/bin:${PATH}"
-export PATH="${RBENV_ROOT}/shims:${PATH}"
-rbenv install 2.1.6
-rbenv global 2.1.6
+install Git git
+install SQLite sqlite3 libsqlite3-dev
+install Curl curl libcurl4-openssl-dev
+install ImageMagick imagemagick libmagickwand-dev
+install NodeJS nodejs
+install Nginx nginx nginx-extras passenger
+install PostgreSQL postgresql postgresql-contrib libpq-dev
+sudo -u postgres createuser --superuser vagrant
+sudo -u postgres createdb -O vagrant activerecord_unittest
+sudo -u postgres createdb -O vagrant activerecord_unittest2
 
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-gem install bundler
-rbenv rehash
-gem install rails
-gem install nokogiri
+echo "gem: --no-ri --no-rdoc" > /etc/gemrc
 
+echo install gems
+gem install bundler ${OUT}
+gem install rails ${OUT}
+gem install nokogiri ${OUT}
+gem install rails -v 4.2.1 ${OUT}
 
-echo installing Ruby on Rails
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-install nodejs
-gem install rails -v 4.2.1
-
-
-echo install PostgreSQL
-sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-install postgresql-common
-install postgresql-9.3 
-install libpq-dev
-sudo -u postgres createuser  User -s
-
-
-
-echo install nginx
-echo Install Phusions PGP key to verify packages
-gpg --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
-gpg --armor --export 561F9B9CAC40B2F7 | sudo apt-key add -
-
-echo Add HTTPS support to APT
-install apt-transport-https
-
-echo Add the passenger repository
-sudo sh -c "echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main' >> /etc/apt/sources.list.d/passenger.list"
-sudo chown root: /etc/apt/sources.list.d/passenger.list
-sudo chmod 600 /etc/apt/sources.list.d/passenger.list
-sudo apt-get update
-
-# Install nginx and passenger
-install nginx-extras passenger
 sudo service nginx start
-
